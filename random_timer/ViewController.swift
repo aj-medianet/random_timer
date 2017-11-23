4ea511e1f1f5d265c371830a71313ebdf25a9480
Andrew Joseph <andrewjoseph@andrews-mbp.local>
2017-11-23 15:18:17 -0700

    Initial Commit

---
+++ random_timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  random_timer
+//
+//  Created by Andrew Joseph on 11/23/17.
+//  Copyright © 2017 AJ Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
